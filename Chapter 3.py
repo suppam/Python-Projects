24be4c1fe71a06c8ac7e7385ff9cc7b5eec8773f
Michael <suppa.michael@gmail.com>
2021-04-29 22:46:02 -0400

    Create Chapter 3.py

---
+++ Chapter 3.py
@@ -0,0 +1,42 @@
+def tripling_value(single_digit):
+    """ (int) -> int
+
+    Return the parameter value single_digit
+    back to the user three times in succession
+    
+    Value entered must be single integer value between 1-9.
+
+    >>> tripling_value (4)
+    444
+
+    >>> tripling_value (8)
+    888
+
+    >>> tripling_value (1)
+    111
+
+    """
+    return (single_digit*100) + (single_digit*10) + (single_digit)
+
+
+
+def times_three(num):
+    """ (int) -> int
+
+    Return a value entered multipled by 3
+
+    >>> times_three(3)
+    9
+
+    >>> times_three(82)
+    246
+
+    >>> time_three(11)
+    33
+
+    """
+
+    return num*3
+
+    
+    
